fb1fa9df0342322a0a27313c113642a50007e547
Thomas <teswolf96@gmail.com>
2015-06-16 12:56:03 -0400

    Added linkedEdges.h

--- nodesAndEdges.c
+++ nodesAndEdges.c
@@ -1,2 +1,26 @@
 #include "linkedEdges.c"
 
+Edge *Create_edge(Node inEnd1, Node inEnd2, int inWeight)
+{
+	Edge *edgeData = malloc(sizeof(struct Edge));
+	edgeData->end1 = inEnd1;
+	edgeData->end2 = inEnd2;
+	edgeData->weight = inWeight;
+	
+	return edgeData;
+}
+
+Node *Create_node(char* newName)
+{
+		Node *nodeData = malloc(sizeof(struct Node));
+		nodeData->name = newName;
+		nodeData->headEdgeNode = NULL;
+		
+		return nodeData;
+}
+
+int Add_edge(Edge addThis, Node addTo)
+{
+
+}
+
